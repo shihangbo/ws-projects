f22fbf0be62306898f7d995073ce2440bb57d59f
shihangbo <shihangbo@126.com>
2021-08-20 16:14:39 +0800

    node

--- packages/ws-webpack/source/tapable/src/index.js
+++ packages/ws-webpack/source/tapable/src/index.js
@@ -7,10 +7,19 @@ let syncLoopHook = require('./SyncLoopHook')
 
 // 异步方法：串行 / 并行 
 // 等待所有的异步方法执行后在执行回调
+let asyncParraleHook = require('./AsyncParalleHook')
+let asyncParraleHookPromise = require('./AsyncParalleHookPromise')
+let asyncSeriesHook = require('./AsyncSeriesHook')
+let asyncSeriesWaterfallHook = require('./AsyncSeriesWaterfallHook')
 
 module.exports = {
   syncHook,
   syncBailHook,
   syncWaterfallHook,
-  syncLoopHook
+  syncLoopHook,
+
+  asyncParraleHook,
+  asyncParraleHookPromise,
+  asyncSeriesHook,
+  asyncSeriesWaterfallHook
 }
